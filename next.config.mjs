eef7c597a3dbfa16e60bd849a88179a2f0c6a8c2
emirusmaximus <cobcompan@gmail.com>
2025-11-08 15:25:09 +0300

    Initialize Next.js configuration file
    Add Next.js configuration with strict mode enabled.

---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+  // ÖNEMLİ: Burada "output: 'export'" OLMAMALI (aksi halde 404'lar olur)
+}
+export default nextConfig
